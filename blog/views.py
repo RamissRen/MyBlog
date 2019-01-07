16efcfe595027961241cebb2612cc9c1f3bdf7df
Kirill Borovskih <borovskih.kirill@rambler.ru>
2019-01-08 04:30:34 +0500

    A template for viewing a separate article has been added.

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
  
 def home(request):
     return render(request, "partial/home.html")
+
+def single(request, id=None):
+    return render(request, "partial/single.html")
